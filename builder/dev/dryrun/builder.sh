7966b47f5e2b1eac2f15792d1227a883408fdf94
jin-python-lc <fromktowntj@yahoo.co.jp>
2022-03-17 09:58:44 +0900

    MOD builder.sh

--- builder/dev/dryrun/builder.sh
+++ builder/dev/dryrun/builder.sh
@@ -3,8 +3,8 @@
 echo ${CODEBUILD_BUILD_ID}
 
 job_name=(${CODEBUILD_BUILD_ID//:/ })
-project_name=(${job_name//-/ })[0]
-deploy_env=(${job_name//-/ })[1]
+project_name=(${job_name//-/ }[0])
+deploy_env=(${job_name//-/ }[1])
 
 echo ${project_name}
 echo ${deploy_env}
