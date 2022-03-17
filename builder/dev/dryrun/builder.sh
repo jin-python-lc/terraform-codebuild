01f64dc4dca8309c8ba5b04231a48c8935eeff87
jin-python-lc <fromktowntj@yahoo.co.jp>
2022-03-17 10:57:43 +0900

    MOD builder.sh to export params

--- builder/dev/dryrun/builder.sh
+++ builder/dev/dryrun/builder.sh
@@ -2,10 +2,10 @@
 job_name=(${CODEBUILD_BUILD_ID//:/ })
 split_build_name=(${job_name//-/ })
 project_name=${split_build_name[0]}
-deploy_env=${split_build_name[1]}
+DEPLOY_ENV=${split_build_name[1]}
 
 echo ${job_name}
 echo ${project_name}
-echo ${deploy_env}
+echo ${DEPLOY_ENV}
 
-export deploy_env
+export DEPLOY_ENV
