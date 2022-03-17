c36bb5988dc36ef730e8cefaed49f2db3bf4c04c
jin-python-lc <fromktowntj@yahoo.co.jp>
2022-03-17 11:38:42 +0900

    MOD buildspec.yml

--- builder/dev/dryrun/builder.sh
+++ builder/dev/dryrun/builder.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 job_name=(${CODEBUILD_BUILD_ID//:/ })
 split_build_name=(${job_name//-/ })
-project_name=${split_build_name[0]}
-deploy_env=${split_build_name[1]}
-
-echo ${job_name}
-echo ${project_name}
-echo ${deploy_env}
-
-DEPLOY_ENV=deploy_env
+DEPLOY_ENV=${split_build_name[1]}
+TFVARS_PATH=./config/${DEPLOY_ENV}/ap-northeast-1.tfvars
 export DEPLOY_ENV
+export TFVARS_PATH
