da16e131703c1f2ea5373251a1084d2c9f7312be
jin-python-lc <fromktowntj@yahoo.co.jp>
2022-03-17 15:15:29 +0900

    MOD iam_users name

--- iam_user.tf
+++ iam_user.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_user" "iam_user" {
     for_each = var.iam_users
-    name = each.value.name
+    name = each.value.name-var.stage.short_name
     path = "/"
     tags = merge(local.tags, map("Name", "${local.system_name}-user"))
 }
