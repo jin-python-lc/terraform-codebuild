8b62950916236526c3c5181d8c2d341dc4596b3a
jin-taito <taito.jin@st-hakky.com>
2022-11-27 10:11:37 +0900

    mod

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.3.5"
+  required_version = "= 1.3.4"
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "~> 3.74.1"
+        version = "= 4.38.0"
     }
   }
 
