21e34f55976df82d3dd4886b7b040795d149b8cc
jin-taito <taito.jin@st-hakky.com>
2022-11-27 10:18:24 +0900

    mod

--- iam_user.tf
+++ iam_user.tf
@@ -2,5 +2,5 @@ resource "aws_iam_user" "iam_user" {
     for_each = var.iam_users
     name = "${each.value.name}-${var.stage.short_name}"
     path = "/"
-    tags = merge(local.tags, map("Name", "${local.system_name}-user"))
+    #tags = merge(local.tags, map("Name", "${local.system_name}-user"))
 }
